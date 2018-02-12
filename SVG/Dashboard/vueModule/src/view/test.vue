15b5df64e85e20ae1cb85ec06f203a67ce3d3278
screetBloom <707885370@qq.com>
2018-02-12 17:55:39 +0800

    this is  a vue module

--- SVG/Dashboard/vueModule/src/view/test.vue
+++ SVG/Dashboard/vueModule/src/view/test.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="test">
-      <svg-dashboard :current-speed="currentSpeed" class="svg_bg"></svg-dashboard>
-      
+      <svg-dashboard :current-speed="currentSpeed"  :level="level" class="svg_bg"></svg-dashboard>
+
       <div style="height: 80px;width: 100%"></div>
 
       <img src="" alt="">
-      
+
     </div>
 </template>
 
@@ -13,35 +13,48 @@
 
   .svg_bg{
     background-color: black;
-
-
-
   }
 
 </style>
 
 <script>
 import svgDashboard from '../components/dashboard.vue'
+
+import svgone from '../components/test.svg'
+
     export default {
         data() {
             return {
-              currentSpeed : 1,
+              currentSpeed : 0,
+              level: 6
             }
         },
         mounted(){
+
+
           setInterval(function () {
-            if(this.currentSpeed==4) {
+            if(this.currentSpeed==(this.level-1)) {
               this.currentSpeed=0
             }else{
               this.currentSpeed++
             }
           }.bind(this),1000)
+
+
         },
         watch: {
 
         },
 		computed: {
-
+      oneImg() {
+        if(this.dateLevel == 1){
+          return oneImg;
+        }else if(this.dateLevel == 2){
+          return twoImg;
+        }else if(this.dateLevel == 3){
+          return threeImg;
+        }
+      },
 		},
         methods: {
 
