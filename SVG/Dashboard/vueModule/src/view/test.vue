9798990b1f94e22600eab495c2cf244916ea6d63
screetBloom <707885370@qq.com>
2018-02-12 11:53:09 +0800

    this is  a vue module

---
+++ SVG/Dashboard/vueModule/src/view/test.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted(){
+
+        },
+        watch: {
+
+        },
+		computed: {
+
+		},
+        methods: {
+
+        },
+        created(){
+
+        },
+		components: {
+
+		},
+    }
+</script>
