603f241afb0651d51d8afc07a09dea3d788f4002
screetBloom <707885370@qq.com>
2018-02-23 16:03:18 +0800

    this is  a vue module

--- SVG/Dashboard/vueModule/src/view/tpl/tpl.vue
+++ SVG/Dashboard/vueModule/src/view/tpl/tpl.vue
@@ -15,8 +15,8 @@
 	export default {
 		data() {
 			return {
-				result:'',
-				test:''
+				result: '',
+				test: ''
 			}
 		},
 		mounted() {
@@ -25,9 +25,9 @@
 				'My skills:' +
 				'{{if(this.showSkills) {}}' +
 				'{{for(var index in this.skills) {}}' +
-				'<br>'+
+				'<br>' +
 				'<a href="#mytpl">{{this.skills[index]}}</a>' +
-				'<br>'+
+				'<br>' +
 				'{{}}}' +
 				'{{} else {}}' +
 				'<p>none</p>' +
@@ -48,29 +48,36 @@
 		watch: {},
 		computed: {},
 		methods: {
-			TemplateEngine(html,options){
-				let reg = /{{(.+?)}}/g ,
+			TemplateEngine(html, options) {
+				let reg = /{{(.+?)}}/g,
 					reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
 					code = 'var r=[];\n',
 					cursor = 0,
 					match;
 
 				/*
-				上述代码添加了一个新的正则表达式, 如果JS代码以if, for, else, switch, case, break, { , }这些内容为起始值,
+				1.  add函数 如果JS代码以if, for, else, switch, case, break, { , }这些内容为起始值,
 				则直接添加该行代码, 不添加到数组中
+				2.  如果不是if、for等关键词，直接添加，但是添加时，对双引号进行替换，全部替换成\"
 				*/
-				var add = function(line, js) {
-					js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+				var add = function (line, js) {
+					js ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
 						(code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
 					return add;
 				}
 
-				while(match = reg.exec(html)) {
-					console.log(cursor,'cursor <<  ',match.index,'match.index << ',html.slice(cursor, match.index))
-					console.log(9999999999999)
+				while (match = reg.exec(html)) {
+					/*
+					遍历传递过来的字符串，对字符串进行处理生成函数体字符串
+					1.  第一个add函数对普通句子进行添加处理
+					2.  返回的add函数对关键词所在句子进行添加处理
+					*/
 					add(html.slice(cursor, match.index))(match[1], true);
 					cursor = match.index + match[0].length
 				}
+				/*
+				本次add函数调用可以删除
+				*/
 				add(html.substr(cursor, html.length - cursor))
 				code += 'return r.join("");'
 
