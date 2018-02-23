62871100136a77f2951bbf5058a158a24bd7160d
screetBloom <707885370@qq.com>
2018-02-23 11:43:56 +0800

    this is  a vue module

--- SVG/Dashboard/vueModule/src/router/index.js
+++ SVG/Dashboard/vueModule/src/router/index.js
@@ -5,16 +5,26 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/test',
-      name: 'test',
-      component: (resolve) => require(['@/view/test'], resolve)
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'HelloWorld',
+			component: HelloWorld
+		},
+		{
+			path: '/tpl',
+			name: 'tpl',
+			component: (resolve) => require(['@/view/tpl/tpl'], resolve)
+		},
+		{
+			path: '/mytpl',
+			name: 'mytpl',
+			component: (resolve) => require(['@/view/tpl/mytpl'], resolve)
+		},
+		{
+			path: '/test',
+			name: 'test',
+			component: (resolve) => require(['@/view/test'], resolve)
+		}
+	]
 })
