f46b0a529c4e958b108451015fcc103b6f7c802e
screetBloom <707885370@qq.com>
2018-02-23 13:08:51 +0800

    this is  a vue module

---
+++ SVG/Dashboard/vueModule/src/view/tpl/mytpl.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+sdfsdfsdfsdfdsfsd
+		<br>
+		<br>
+		<div style="height: 80px;width: 100%;background-color: #42b983"></div>
+	</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+	export default {
+		data() {
+			return {}
+		},
+		mounted() {
+			let template =
+				'My skills:' +
+				'{{if(this.showSkills) {}}' +
+				'{{for(var index in this.skills) {}}' +
+				'<br>'+
+				'<a href="#test">{{this.skills[index]}}</a>' +
+				'<br>'+
+				'{{}}}' +
+				'{{} else {}}' +
+				'<p>none</p>' +
+				'{{}}}';
+
+
+
+
+		},
+		watch: {},
+		computed: {
+
+		},
+		methods: {
+
+		},
+		created() {
+
+		},
+		components: {},
+	}
+</script>
