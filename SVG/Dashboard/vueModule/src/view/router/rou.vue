1526e82c72fbdb4ac112248e1ae715e8ace30fa4
screetBloom <707885370@qq.com>
2018-02-24 10:02:16 +0800

    this is  a vue module

---
+++ SVG/Dashboard/vueModule/src/view/router/rou.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+
+
+
+	</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+	export default {
+		data() {
+			return {}
+		},
+		mounted() {
+
+		},
+		watch: {},
+		computed: {},
+		methods: {},
+		created() {
+
+		},
+		components: {},
+	}
+</script>
