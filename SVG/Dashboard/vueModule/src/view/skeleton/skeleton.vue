2ef93a77ed0cd331673ec42c2cb8fe452fb6988b
screetBloom <15306225869@163.com>
2018-03-06 16:46:52 +0800

    this is  a vue module

---
+++ SVG/Dashboard/vueModule/src/view/skeleton/skeleton.vue
@@ -0,0 +1,27 @@
+/**
+*
+* User: wim_chen
+* Date: 2018/3/6
+* Time: 下午4:22
+*
+*/
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+
+
+</style>
