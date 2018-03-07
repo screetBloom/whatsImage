42191e9723ab0310670b916c4be40e172c44cfeb
screetBloom <707885370@qq.com>
2018-03-07 16:55:28 +0800

    skeleton

--- SVG/Dashboard/vueModule/src/view/skeleton/skeleton.vue
+++ SVG/Dashboard/vueModule/src/view/skeleton/skeleton.vue
@@ -1,35 +1,14 @@
-
 <template>
-
-	  <svg
-		  height="115"
-		  width="375"
-		  viewBox="0 0 375 115"
-	  >
-		  <!-- 圆角矩形背景 -->
-		  <rect x="5" y="2.5" rx="2" ry="2" width="365" height="110" style="fill:rgb(255,255,255);stroke:none;"/>
-
-		  <!-- 左边第1列 -->
-		  <rect x="25" y="25" width="62" height="18" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="25" y="55" width="69" height="13" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="25" y="92" width="110" height="10" style="fill:rgb(245,245,245);stroke:none;"/>
-
-		  <!-- 左边第2列 -->
-		  <rect x="127" y="32" width="40" height="10" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="114" y="46" width="67" height="6" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="135" y="56" width="25" height="10" style="fill:rgb(245,245,245);stroke:none;"/>
-
-		  <!-- 左边第3列 -->
-		  <rect x="210" y="25" width="63" height="18" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="203" y="55" width="69" height="13" style="fill:rgb(245,245,245);stroke:none;"/>
-
-		  <!-- 左边第4列 -->
-		  <rect x="297" y="28" width="56" height="15" style="fill:rgb(245,245,245);stroke:none;"/>
-		  <rect x="297" y="53" width="55" height="14" style="fill:rgb(245,245,245);stroke:none;"/>
-
-
-	  </svg>
-
-
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlnsXlink="http://www.w3.org/1999/xlink">
+		<defs>
+			<symbol viewBox="0 0 26 31" id="location">
+				<path fill="#FFF" fillRule="evenodd" d="M22.116 22.601c-2.329 2.804-7.669 7.827-7.669 7.827-.799.762-2.094.763-2.897-.008 0 0-5.26-4.97-7.643-7.796C1.524 19.8 0 16.89 0 13.194 0 5.908 5.82 0 13 0s13 5.907 13 13.195c0 3.682-1.554 6.602-3.884 9.406zM18 13a5 5 0 1 0-10 0 5 5 0 0 0 10 0z"></path>
+			</symbol>
+			<symbol viewBox="0 0 14 8" id="arrow">
+				<path fill="#FFF" fillRule="evenodd" d="M5.588 6.588c.78.78 2.04.784 2.824 0l5.176-5.176c.78-.78.517-1.412-.582-1.412H.994C-.107 0-.372.628.412 1.412l5.176 5.176z"></path>
+			</symbol>
+		</defs>
+	</svg>
 </template>
-
