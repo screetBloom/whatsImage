680fd6b8e8572944c2eb891ed740780245dac0f8
screetBloom <15306225869@163.com>
2018-03-29 09:01:00 +0800

    Merge branch 'master' of https://github.com/screetBloom/whatsImage
    # Conflicts:
    #	.idea/workspace.xml

--- SVG/Dashboard/vueModule/src/view/skeleton/ske.vue
+++ SVG/Dashboard/vueModule/src/view/skeleton/ske.vue
@@ -1,56 +1,43 @@
 <template>
-	<div style="background-color: black">
+  <div style="background-color: black">
 
 
-
-		<div style="height: 50px;width: 100%"></div>
-
+    <div style="height: 50px;width: 100%"></div>
 
 
-
-
-
-
-
-		<div style="width: 375px;height: 1150px" class="test"></div>
-	</div>
+    <div style="width: 375px;height: 1150px" class="test"></div>
+  </div>
 </template>
 
 <style lang="less" type="text/less">
 
-	@test-svg:"data:image/svg+xml,%3Csvg height='115' width='375' viewBox='0 0 375 115' xmlns='http://www.w3.org/2000/svg' version='1.1'%3E%3Crect x='5' y='2.5' rx='2' ry='2' width='365' height='110' style='fill:rgb%28255,255,255%29;stroke:none;'/%3E%3Crect x='25' y='25' width='62' height='18' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='25' y='55' width='69' height='13' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='25' y='92' width='110' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='127' y='32' width='40' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='114' y='46' width='67' height='6' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='135' y='56' width='25' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='210' y='25' width='63' height='18' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='203' y='55' width='69' height='13' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='297' y='28' width='56' height='15' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='297' y='53' width='55' height='14' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3C/svg%3E";
+  @test-svg: "data:image/svg+xml,%3Csvg height='115' width='375' viewBox='0 0 375 115' xmlns='http://www.w3.org/2000/svg' version='1.1'%3E%3Crect x='5' y='2.5' rx='2' ry='2' width='365' height='110' style='fill:rgb%28255,255,255%29;stroke:none;'/%3E%3Crect x='25' y='25' width='62' height='18' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='25' y='55' width='69' height='13' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='25' y='92' width='110' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='127' y='32' width='40' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='114' y='46' width='67' height='6' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='135' y='56' width='25' height='10' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='210' y='25' width='63' height='18' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='203' y='55' width='69' height='13' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='297' y='28' width='56' height='15' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3Crect x='297' y='53' width='55' height='14' style='fill:rgb%28245,245,245%29;stroke:none;'/%3E%3C/svg%3E";
 
+  .test {
+    background-image: url(@test-svg);
+    background-size: 375px 115px;
+    background-repeat: repeat;
 
-
-	.test{
-		background-image: url(@test-svg);
-		background-size: 375px 115px;
-		background-repeat: repeat;
-
-	}
+  }
 
 </style>
 
 <script>
 
 
-	export default {
-		data() {
-			return {}
-		},
-		mounted() {
+  export default {
+    data() {
+      return {}
+    },
+    mounted() {
 
-		},
-		watch: {},
-		computed: {
+    },
+    watch: {},
+    computed: {},
+    methods: {},
+    created() {
 
-		},
-		methods: {},
-		created() {
-
-		},
-		components: {
-
-		},
-	}
+    },
+    components: {},
+  }
 </script>
