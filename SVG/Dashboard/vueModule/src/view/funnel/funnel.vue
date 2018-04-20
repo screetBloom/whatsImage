28815af77b2b57878a27e25a44a3aefe6cdffc6c
screetBloom <15306225869@163.com>
2018-04-20 10:38:07 +0800

    修复无法运行的问题,原因是原本是把ip写死了

---
+++ SVG/Dashboard/vueModule/src/view/funnel/funnel.vue
@@ -0,0 +1,24 @@
+/**
+*
+* User: wim_chen
+* Date: 2018/4/20
+* Time: 上午10:37
+*
+*/
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
